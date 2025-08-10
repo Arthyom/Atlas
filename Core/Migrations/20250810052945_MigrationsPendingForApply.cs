2e678cc2fee5f19c1affb33a25013ce20202a1d0
arthyom <a.gonzalezgavina@ugto.mx>
2025-08-09 23:32:35 -0600

    Adding pending changes on migration pipelines

---
+++ Core/Migrations/20250810052945_MigrationsPendingForApply.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationsPendingForApply : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Producto",
+                type: "varchar(100)",
+                unicode: false,
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Producto");
+        }
+    }
+}
