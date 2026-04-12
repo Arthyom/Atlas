0fda1c6296376a45b426b02d5768149d8ac6b0c1
Arthyom <a.gonzalezgavina@ugto.mx>
2026-04-12 01:02:44 -0600

    Replacing multi 'producto' approach by 'modelo' approaching for item's variations and stilying swippers

--- Web/ClientApp/src/Pages/Shop/components/ShopProductDetail.vue
+++ Web/ClientApp/src/Pages/Shop/components/ShopProductDetail.vue
@@ -3,12 +3,19 @@ import { useAtlasComposableUseFilesFetcher } from "@/Models/Composables/AtlasCom
 import { IDtoProducto } from "@/Pages/Producto/DTOs/IDtoProducto";
 import IAtlasMixedResponse from "@/Models/Interfaces/IAtlasMixedResponse";
 import { useAtlasCartStore } from "@/Pages/Shared/store/AtlasCartStore";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { useAtlasComposableRouterHtmlActions } from "@/Models/Composables/AtlasComposableRouterActions";
 
+import SwiperClass from 'swiper';
+
 import { Swiper, SwiperSlide } from "swiper/vue";
+import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
 
 import "swiper/css";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
 
 const props = defineProps<{ producto: IDtoProducto; showOptions?: boolean }>();
 
@@ -23,10 +30,12 @@ const { length, addProduct } = useAtlasCartStore();
 const isVisible = ref(false);
 const { getHtml } = useAtlasComposableRouterHtmlActions();
 
-const defImage = ref(props.producto?.imagenes?.[0] || '');
-const itemSelector = ref<HTMLDivElement [] | null>(null);
+const imageIndexSelected = ref(0);
 
-const setSelectedImage = (event:any, image: string) => {
+const defImage = ref(props.producto?.imagenes?.[0] || "");
+const itemSelector = ref<HTMLDivElement[] | null>(null);
+
+const setSelectedImage = (event: any, image: string) => {
   const imageContainer = event.currentTarget.firstChild as HTMLDivElement;
   const itemsList = itemSelector.value;
 
@@ -36,8 +45,11 @@ const setSelectedImage = (event:any, image: string) => {
 
   imageContainer.classList.add("ring-primary");
   defImage.value = image;
-
 };
+
+const selectedModel = computed(
+  () => props.producto.modelos?.[imageIndexSelected.value] || null,
+);
 </script>
 
 <template>
@@ -46,33 +58,48 @@ const setSelectedImage = (event:any, image: string) => {
     @mouseenter="isVisible = true"
     @mouseleave="isVisible = false"
   >
-  <div class="flex px-3 py-2 text-left">
-        <span class="flex-1 text-2xl font-extralight ">{{
-          props.producto.nombre
-        }}</span>
-        <span class="text-2xl font-bold " >{{
-          $n( props.producto.precioUnitario, 'currency' )
-        }}</span>
-      </div>
-    <figure class=" p-2">
-      <template v-if="props.producto.imagenes">
-        <img
-          :src="
-            getFileFrom(
-              'productoAnonimous',
-              'store',
-              defImage,
-            )
-          "
-          class="min-h-110 max-h-110 w-full object-fill"
-        />
-      </template>
-      <template v-else>
-        <img :src="noFile()" class="min-h-60 max-h-60 w-full" />
-      </template>
-    </figure>
+    <div class="flex px-3 py-2 text-left">
+      <span class="flex-1 text-2xl font-extralight">{{
+        props.producto.nombre
+      }}</span>
+      <span class="text-2xl font-bold">{{
+        $n(props.producto.precioUnitario, "currency")
+      }}</span>
+    </div>
 
-    <transition
+    <div>
+      <swiper :slides-per-view="1" :space-between="1" :navigation="true" :modules="[Navigation]" >
+        <template v-if="selectedModel">
+          <swiper-slide
+            v-for="imageModel in selectedModel?.imagenes"
+            :key="imageModel"
+            class="px-6 "
+          >
+            <figure class="p-2">
+              <div v-if="imageModel">
+                <img
+                  :src="getFileFrom('ModeloAnonimous', 'store', imageModel)"
+                  class="min-h-110 max-h-110 w-full object-fill"
+                />
+              </div>
+              <div v-else>
+                <img :src="noFile()" class="min-h-60 max-h-60 w-full" />
+              </div>
+            </figure>
+          </swiper-slide>
+        </template>
+        <template v-else>
+
+          <swiper-slide class="p-1">
+            <figure class="p-2">
+              <img :src="noFile()" class="min-h-60 max-h-60 w-full" />
+            </figure>
+          </swiper-slide>
+        </template>
+      </swiper>
+    </div>
+
+    <!-- <transition
       enter-active-class="transition-opacity duration-300 ease-out"
       leave-active-class="transition-opacity duration-300 ease-in"
       enter-from-class="opacity-0"
@@ -89,26 +116,40 @@ const setSelectedImage = (event:any, image: string) => {
               <font-awesome-icon icon="fas fa-dollar"></font-awesome-icon>
             </button>
             <button class="btn btn-circle" @click="addProduct(props.producto)">
-              <font-awesome-icon icon="fas fa-cart-arrow-down"></font-awesome-icon>
+              <font-awesome-icon
+                icon="fas fa-cart-arrow-down"
+              ></font-awesome-icon>
             </button>
           </div>
         </div>
       </div>
-    </transition>
+    </transition> -->
 
-    
     <div class="flex justify-center p-0 m-0">
       <img src="/logo.png" class="min-h-22 max-h-22" />
     </div>
 
     <div class="">
-
       <div class="flex flex-row">
-        <swiper :navigation="true" :slides-per-view="4" :space-between="0">
-          <swiper-slide v-for="image in props.producto.imagenes" :key="image" class="p-1 ">
-            <div @click="setSelectedImage($event,image)" class="avatar">
-              <div ref="itemSelector" class="item-selector w-20 rounded-full m-2  ring-gray-400 ring-3 ring-offset-2">
-                <img :src="getFileFrom('productoAnonimous', 'store', image)" />
+        <swiper :pagination="true" :slides-per-view="4" :space-between="0" >
+          <swiper-slide
+            v-for="(imageModel, index) in props.producto.modelos || []"
+            :key="index"
+            class="p-1 pb-8"
+          >
+
+            <div
+              @click="
+                setSelectedImage($event, imageModel.imagenes?.[0] || '');
+                imageIndexSelected = index;
+              "
+              class="avatar"
+            >
+              <div
+                ref="itemSelector"
+                class="item-selector w-20 rounded-full m-2 ring-gray-400 ring-3 ring-offset-2"
+              >
+                <img :src="getFileFrom('ModeloAnonimous', 'store', imageModel.imagenes?.[0] || '')" />
               </div>
             </div>
           </swiper-slide>
