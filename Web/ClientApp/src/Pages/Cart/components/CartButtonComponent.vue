91b5543ebf45fdd3527b4a0862851a4fdb224b97
Carlos Orozco <17306416+CarlosOrozcoM@users.noreply.github.com>
2026-05-02 11:45:32 -0600

    Refactor: Update cart summary layout

--- Web/ClientApp/src/Pages/Cart/components/CartButtonComponent.vue
+++ Web/ClientApp/src/Pages/Cart/components/CartButtonComponent.vue
@@ -15,9 +15,9 @@ const goToCart =() =>{
 
 <template>
   
-  <template v-if="length > 0">
+    <template v-if="length > 0">
 
-      <!-- <div  @click="goto" tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <!-- <div  @click="goto" tabindex="0" role="button" class="btn btn-ghost btn-circle">
     
     <div class="indicator">
       <svg
@@ -43,9 +43,9 @@ const goToCart =() =>{
 
 
 
-    <div class="dropdown dropdown-start">
-      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-        <div class="indicator">
+        <div class="dropdown dropdown-start">
+            <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+                <div class="indicator">
            <svg
         xmlns="http://www.w3.org/2000/svg"
         class="h-7 w-7"
@@ -53,45 +53,34 @@ const goToCart =() =>{
         viewBox="0 0 24 24"
         stroke="currentColor"
       >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-        />
-      </svg>
-          <span class="badge badge-sm badge-soft indicator-item">{{ totalProductos }}</span>
-        </div>
-      </div>
-      <div
-        tabindex="0"
-        class="card card-compact dropdown-content bg-base-300 z-18 mt-3 w-52 h-80 shadow">
-        <div class="card-body p-2">
-          <div class="card-title">
-            <div class="flex w-full ">
-                    
-                        <span class="text-info font-bold text-center text-2xl flex-1 self-center">${{ totalPago }}</span>
-                        <button @click="goToCart" class="btn btn-sm btn-primary self-center">
-                          Pagar
-                        </button>
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                        />
+                    </svg>
+                    <span class="badge badge-sm badge-soft indicator-item">{{ totalProductos }}</span>
+                </div>
             </div>
-          </div>
+            <div 
+                tabindex="0"
+                class="card card-compact dropdown-content bg-base-100 z-18 mt-3 w-72 h-120 shadow">
+                <div class="card-body p-2 flex flex-col h-full overflow-hidden">
+                    <div class="card-title shrink-0">
+                        <div class="flex w-full">
+                            <span class="font-bold text-center text-2xl flex-1">${{ totalPago }}</span>
+                            <button @click="goToCart" class="btn btn-sm btn-neutral">Pagar</button>
+                        </div>
+                    </div>
 
-          <div class="divider m-0"></div>
-
-          <div class="cardody">
-            <div class="grid grid-cols-2 gap-y-3">
-              <template v-for="p in products">
-                
-                <CartProductViewer v-bind="p"/>
-              </template>
+                    <div class="flex-1 overflow-y-auto">
+                        <template v-for="p in products">
+                            <CartProductViewer v-bind="p"/>
+                        </template>
+                    </div>
+                </div>
             </div>
-          </div>
-          
         </div>
-      </div>
-    </div>
-
-  </template>
-
+    </template>
 </template>
--- Web/ClientApp/src/Pages/Cart/components/CartProductViewer.vue
+++ Web/ClientApp/src/Pages/Cart/components/CartProductViewer.vue
@@ -11,54 +11,53 @@ const { increase, decrease, restore, exclude } = useAtlasCartStore();
 </script>
 
 <template>
-  <div class="">
-    <div class="flex flex-col m-0 justify-center">
-      <div class="flex ">
-        <div class="absolute z-10">
-          <button
-          :class="{
-            'btn-error' : props.selected,
-            'btn-success' : !props.selected
-          }"
-            class="btn btn-sm  md:btn-sm lg:btn-md btn-base-100 btn-circle items-center"
-          >
-            {{ props.cantidad }}
-          </button>
-        </div>
-        <div class="flex w-full justify-center">
-          <div class="avatar flex justify-center">
-            <div class="w-18 rounded-full">
-              <img :src="getFirstFileForProductoAnonimous(props)" />
+    <div class="flex flex-col py-2 justify-center">
+        <div class="divider m-0 shrink-0"></div> 
+        <div class="flex relative">
+            <div class="absolute z-10 left-10">
+                <button
+                    :class="{
+                        'btn-error': props.selected,
+                        'btn-success': !props.selected,
+                    }"
+                    class="btn btn-sm md:btn-sm lg:btn-md btn-circle items-center"
+                >
+                    {{ props.cantidad }}
+                </button>
             </div>
-          </div>
+
+            <div class="flex w-full justify-center">
+                <div class="avatar flex justify-center">
+                    <div class="w-36 rounded-full">
+                        <img :src="getFirstFileForProductoAnonimous(props)" />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
 
-      <div class="join justify-center" v-if="!props.selected">
-        <button
-          @click="increase(props)"
-          class="btn btn-xs join-item rounded-l-full"
-        >
-          <font-awesome-icon icon="fas fa-plus" size="xs" />
-        </button>
-        <button
-          @click="decrease(props)"
-          class="btn btn-xs join-item rounded-r-full"
-        >
-          <font-awesome-icon icon="fas fa-minus" size="xs" />
-        </button>
+        <div class="join justify-center" v-if="!props.selected">
+            <button
+                @click="increase(props)"
+                class="btn btn-sm join-item rounded-l-full">
+                        <font-awesome-icon icon="fas fa-plus" size="xl" />
+                    </button>
+            <button
+                @click="decrease(props)"
+                class="btn btn-sm join-item rounded-r-full">
+                    <font-awesome-icon icon="fas fa-minus" size="xl" />
+            </button>
 
-        <button @click="exclude(props)" 
-        class="btn btn-xs join-item rounded-full ml-2">
-          <font-awesome-icon icon="fas fa-trash" size="xs" />
-        </button>
-      </div>
+            <button 
+                @click="exclude(props)" 
+                class="btn btn-sm join-item rounded-full ml-2">
+                    <font-awesome-icon icon="fas fa-trash" size="lg" />
+            </button>
+        </div>
 
-      <div v-else class="flex justify-center">
-        <button @click="restore(props)" class="btn btn-xs btn-circle ml-2">
-          <font-awesome-icon icon="fas fa-circle" size="xs" />
-        </button>
-      </div>
+        <div v-else class="flex justify-center">
+            <button @click="restore(props)" class="btn btn-xs btn-circle ml-2">
+                <font-awesome-icon icon="fas fa-circle" size="xs" />
+            </button>
+        </div>
     </div>
-  </div>
 </template>
